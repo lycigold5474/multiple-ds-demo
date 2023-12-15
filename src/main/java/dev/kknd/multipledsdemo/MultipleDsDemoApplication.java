39ea33da2205b6ee61c486eb479c468ae8a54a09
andy <lycigold@gmail.com>
2023-12-16 07:39:08 +0900

    spring 3.2 multiple datasource demo init

---
+++ src/main/java/dev/kknd/multipledsdemo/MultipleDsDemoApplication.java
@@ -0,0 +1,13 @@
+package dev.kknd.multipledsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultipleDsDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultipleDsDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/kknd/multipledsdemo/MultipleDsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kknd.multipledsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultipleDsDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
