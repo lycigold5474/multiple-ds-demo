df863ab3fdaadff2b7802aaa5fc54075909a0277
andy <lycigold@gmail.com>
2023-12-16 09:34:01 +0900

    SubscriberService 데이터소스설정 및 조회 설정

--- src/main/java/dev/kknd/multipledsdemo/subscriber/SubscriberService.java
+++ src/main/java/dev/kknd/multipledsdemo/subscriber/SubscriberService.java
@@ -1,7 +1,23 @@
 package dev.kknd.multipledsdemo.subscriber;
 
+import dev.kknd.multipledsdemo.post.Post;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class SubscriberService {
+
+    private JdbcClient jdbcClient;
+    public SubscriberService(@Qualifier("subscriberJdbcClient") JdbcClient jdbcClient) {
+        this.jdbcClient = jdbcClient;
+    }
+
+    public List<Subscriber> findAll() {
+        return jdbcClient.sql("SELECT id, name, email FROM Subscriber")
+                .query(Subscriber.class)
+                .list();
+    }
 }
